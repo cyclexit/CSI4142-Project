ab5f8fe5a6242cf0f5b8c92b7544972251174f8b
cyclexit <cyclexit@outlook.com>
2022-03-18 11:29:16 -0400

    Add csv file relative paths to constant.py and use the constants

--- phase_2/scripts/constants.py
+++ phase_2/scripts/constants.py
@@ -1,3 +1,5 @@
+import os
+
 # NOTE: all schema names and table names are all in lower-case in postgresql
 SCHEMA_NAME = "csi4142_project"
 FACT_TABLE = "fact_table"
@@ -8,3 +10,8 @@ HEALTH_DIMENSION = "health_dimension"
 LIFE_QUALITY_DIMENSION = "life_quality_dimension"
 POPULATION_DIMENSION = "population_dimension"
 YEAR_DIMENSION = "year_dimension"
+
+# csv relative file path
+PATH_PREFIX = "../data/"
+POPULATION_DIMENSION_CSV = os.path.join(PATH_PREFIX, "population_dimension.csv")
+YEAR_DIMENSION_CSV = os.path.join(PATH_PREFIX, "year_dimension.csv")
