d4074b877631561ae42227d88f0ae7e7cbd9094d
cyclexit <cyclexit@outlook.com>
2022-03-17 20:28:32 -0400

    Upload constants.py

---
+++ phase_2/scripts/constants.py
@@ -0,0 +1,10 @@
+# NOTE: all schema names and table names are all in lower-case in postgresql
+SCHEMA_NAME = "csi4142_project"
+FACT_TABLE = "fact_table"
+COUNTRY_DIMENSION = "country_dimension"
+EDUCATION_DIMENSION = "education_dimension"
+EVENT_DIMENSION = "event_dimension"
+HEALTH_DIMENSION = "health_dimension"
+LIFE_QUALITY_DIMENSION = "life_quality_dimension"
+POPULATION_DIMENSION = "population_dimension"
+YEAR_DIMENSION = "year_dimension"
