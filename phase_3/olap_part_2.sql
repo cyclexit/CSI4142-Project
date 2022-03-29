7365db2e1c445d8b4e06f126245d626af16ed9ed
cyclexit <cyclexit@outlook.com>
2022-03-29 18:04:40 -0400

    Finish the iceberg query

--- phase_3/olap_part_2.sql
+++ phase_3/olap_part_2.sql
@@ -1,8 +1,19 @@
+set search_path = "csi4142_project";
+
 /**
  * Part 2. a. Iceberg query
- * 
+ * Top 3 countries with the highest average human development index in 2010's
  */
-
+select C.country_name, Y.decade, avg(F.human_development_index) as avg_hdi
+from country_dimension as C,
+    year_dimension as Y,
+    Fact_Table as F
+where F.year_key = Y.year_key
+    and F.country_key = C.country_key
+    and Y.decade = 2010
+group by (C.country_name, Y.decade)
+order by avg(F.human_development_index) desc
+limit 3;
 
 /**
  * Part 2. b. Windowing query
