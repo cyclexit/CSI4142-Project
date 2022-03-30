2d732da14ce580b24208cfd2e4eccb6438def11b
cyclexit <cyclexit@outlook.com>
2022-03-29 20:00:14 -0400

    Finish windowing query

--- phase_3/olap_part_2.sql
+++ phase_3/olap_part_2.sql
@@ -17,9 +17,21 @@ limit 3;
 
 /**
  * Part 2. b. Windowing query
- * 
+ * Compare the human development index of each country in Asia in 2015 with the
+ * average and provide a rank.
  */
-
+select C.country_name,
+	C.continent,
+	F.human_development_index,
+    avg(F.human_development_index) over (partition by C.continent) as continent_avg_hdi,
+	rank() over (partition by C.continent order by F.human_development_index desc)
+from country_dimension as C,
+    year_dimension as Y,
+    Fact_Table as F
+where F.year_key = Y.year_key
+    and F.country_key = C.country_key
+    and Y.year_num = 2015
+group by (C.country_name, C.continent, F.human_development_index);
 
 /**
  * Part 2. c. Using the Window clause
