71773e3b9a214b0cc92c0de59f285b9e86f56500
cyclexit <cyclexit@outlook.com>
2022-03-29 21:19:16 -0400

    Finish the windowing query using window clause and part 2 is done

--- phase_3/olap_part_2.sql
+++ phase_3/olap_part_2.sql
@@ -35,5 +35,19 @@ group by (C.country_name, C.continent, F.human_development_index);
 
 /**
  * Part 2. c. Using the Window clause
- * 
+ * The moving average of total population over 3 years of each country.
  */
+
+select C.country_name, C.continent, Y.year_num, P.total_population, avg(P.total_population) over W as moving_avg_total_population
+from country_dimension as C,
+    year_dimension as Y,
+    population_dimension as P,
+    Fact_Table as F
+where F.country_key = C.country_key
+    and F.year_key = Y.year_key
+    and F.population_key = P.population_key
+window W as (
+    partition by C.country_name
+    order by Y.year_num
+    rows between 1 preceding and 1 following
+);
